75cd6dd074682125cf28cae974801134cf60ec4a
Sai Koushik Gandikota <saikoushik.gsk1@gmail.com>
2023-03-06 11:21:26 -0500

    changed region name from APAC to NAMER
    first modification in Query 1

--- Query 1.sql
+++ Query 1.sql
@@ -3,4 +3,4 @@ Select * From gdb023.dim_customer;
 -- Provide the list of markets in which customer "Atliq Exclusive" operates its
 -- business in the APAC region.
 
-SELECT market from gdb023.dim_customer where customer="Atliq Exclusive" and region="APAC";
+SELECT market from gdb023.dim_customer where customer="Atliq Exclusive" and region="NAMER";
